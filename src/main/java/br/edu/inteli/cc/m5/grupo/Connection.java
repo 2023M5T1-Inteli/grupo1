06c0735858f934a783a9acf51ac64a9a027cc085
mateusalm <mateus.almeida@sou.inteli.edu.br>
2023-02-26 17:53:44 -0300

    comentários adicionados aos códigos

--- src/main/java/br/edu/inteli/cc/m5/grupo/Connection.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo/Connection.java
@@ -19,11 +19,11 @@ import br.edu.inteli.cc.m5.dted.DtedDatabaseHandler;
 
 public class Connection {
     private final Driver driver;
-    
-    private final String uri = "neo4j+s://6c96b965.databases.neo4j.io:7687";
-    private final String user = "neo4j";
-    private final String password = "Gk54tBkc3WXygiR9X9U6GMWoaB9GbvbogYFyq7YpsqA";
-    private final Config config = Config.defaultConfig();
+
+    private final String uri = "NEO4J_URI"; // endereço do banco de dados Neo4j
+    private final String user = "NEO4J_USER"; // usuário de acesso do DB
+    private final String password = "NEO4J_PSW"; // senha de acesso do DB
+    private final Config config = Config.defaultConfig(); // configura a conexão com o DB
 
     public Connection() {
         driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password), config);
@@ -31,17 +31,26 @@ public class Connection {
 
     // ---------- NODES -----------
 
+    /**
+     * Função que cria (CREATE) um Vértice novo no Neo4j
+     * 
+     * @param id        id do nó
+     * @param latitude  latitude do Nó
+     * @param longitude logitude do Nó
+     * @param altitude  altitude do Nó
+     */
     public void createNode(int id, double latitude, double longitude, double altitude) {
         var query = new Query(
-            """
-                CREATE (n:Node { id: $id, lat: $latitude, long: $longitude, alt: $altitude })
-                RETURN n
-            """,
-                Map.of("latitude", latitude, "longitude", longitude, "altitude", altitude, "id", id));
-
+                """
+                            CREATE (n:Node { id: $id, lat: $latitude, long: $longitude, alt: $altitude })
+                            RETURN n
+                        """,
+                Map.of("latitude", latitude, "longitude", longitude, "altitude", altitude, "id", id)); // query de
+                                                                                                       // criação de Nó
+                                                                                                       // no
+                                                                                                       // banco de dados
 
         try (var session = driver.session(SessionConfig.forDatabase("neo4j"))) {
-            // Write transactions allow the driver to handle retries and transient errors
             var record = session.executeWrite(tx -> tx.run(query).single());
             System.out.printf(
                     "Created node id:%d, lat:%.2f , long:%.2f , alt:%.2f",
@@ -49,24 +58,29 @@ public class Connection {
                     record.get("n").get("lat").asDouble(),
                     record.get("n").get("long").asDouble(),
                     record.get("n").get("alt").asDouble());
-            // You should capture any errors along with the query and data for traceability
         } catch (Neo4jException ex) {
             throw ex;
 
         }
     }
 
+    /**
+     * Função que lê (READ) as informações de um Vértice existente no Neo4j com base
+     * n o id
+     * 
+     * @param id id do Nó
+     * @return
+     */
     public Record readNode(int id) {
         var query = new Query(
-            """  
-            MATCH (n:Node)
-            WHERE n.id = $id
-            RETURN n
-            """,
-            Map.of("id", id));
+                """
+                        MATCH (n:Node)
+                        WHERE n.id = $id
+                        RETURN n
+                        """,
+                Map.of("id", id)); // query de busca no banco de dados
 
         try (var session = driver.session(SessionConfig.forDatabase("neo4j"))) {
-            // Write transactions allow the driver to handle retries and transient errors
             Record record = session.executeWrite(tx -> tx.run(query).single());
             System.out.printf(
                     "Read node id:%d, lat:%.2f , long:%.2f , alt:%.2f",
@@ -82,10 +96,20 @@ public class Connection {
         }
     }
 
+    /**
+     * Função que atualiza (UPDATE) as informações de um Vértice no banco de dados
+     * 
+     * @param id         id do Nó cujos dados serão modificados
+     * @param alt        nova altitude do Nó
+     * @param lat        nova latitude do Nó
+     * @param longParams nova logitude do Nó
+     */
     public void updateNode(int id, Double alt, Double lat, Double longParams) {
-        try(var session = driver.session()) {
-            try(var tx = session.beginTransaction()) {
-                tx.run("MATCH (n: Node {id: " + id + " }) SET n.alt = " + alt + ", n.lat = " + lat + ", n.long = " + longParams);
+        try (var session = driver.session()) {
+            try (var tx = session.beginTransaction()) {
+                tx.run("MATCH (n: Node {id: " + id + " }) SET n.alt = " + alt + ", n.lat = " + lat + ", n.long = "
+                        + longParams); // query que busca um Nó com base no id no banco de dados e muda suas
+                                       // informações
 
                 tx.commit();
             }
@@ -94,11 +118,16 @@ public class Connection {
         }
     }
 
-    // Deleta um nó e suas respectivas relações
+    /**
+     * Função que apaga (DELETE) um Vértice no banco de dados com base no id
+     * 
+     * @param id id do Nó a ser deletado
+     */
     public void deleteNode(int id) {
         Session session = driver.session();
 
-        String query = "MATCH (n:Person {name: 'Carrie-Anne Moss'}) DETACH DELETE n";
+        String query = "MATCH (n:Person {name: 'Carrie-Anne Moss'}) DETACH DELETE n"; // query que apaga um nó com base
+                                                                                      // no id
 
         Map<String, Object> parameters = new HashMap<>();
 
@@ -112,12 +141,21 @@ public class Connection {
     }
 
     // ---------- RELATIONSHIPS -----------
+
+    /**
+     * Função que cria (CREATE) uma Aresta nova entre dois Vértices existentes no
+     * Neo4j
+     * 
+     * @param id1    id do Nó de origem
+     * @param id2    id do Nó de destino
+     * @param dist   distância entre os Nós
+     * @param altVar variação da altitude entre os Nós
+     */
     public void createRelationshipTwoNodes(int id1, int id2, double dist, double altVar) {
         Session session = driver.session();
 
         String cypherQuery = "MATCH (a:Node {id: $nodeAId}), (b:Node {id: $nodeBId}) CREATE (a)-[:GO_TO {dist: $distance, alt_var: $altVar}]->(b)";
 
-        // Define the parameters for the query
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("nodeAId", id1);
         parameters.put("nodeBId", id2);
@@ -129,18 +167,17 @@ public class Connection {
         session.close();
     }
 
-
     public void createRelationship(int n) {
 
-        for (int i = 0; i < n*n; i++) {
+        for (int i = 0; i < n * n; i++) {
 
             Record record = readNode(i);
 
             double lat1 = record.get("n").get("lat").asDouble();
             double lon1 = record.get("n").get("long").asDouble();
             double alt1 = record.get("n").get("alt").asDouble();
-            
-            int[] idsToConnect = {i + 1, i + n, i + n + 1, i + n + - 1};
+
+            int[] idsToConnect = { i + 1, i + n, i + n + 1, i + n + -1 };
 
             for (int j = 0; j < idsToConnect.length; j++) {
                 int id2 = idsToConnect[j];
@@ -155,44 +192,26 @@ public class Connection {
                 double lon_diff = lon2 - lon1;
                 double dist = Math.sqrt(Math.pow(lat_diff, 2) + Math.pow(lon_diff, 2)) * 111.1;
 
-                if (dist <= 120*1.41) {
-                    createRelationshipTwoNodes(i, id2, dist, alt2-alt1);
+                if (dist <= 120 * 1.41) {
+                    createRelationshipTwoNodes(i, id2, dist, alt2 - alt1);
                 }
             }
-            }
         }
+    }
 
-        /*
-            . . . .
-            . . . .
-            . . . .
-            . . . .
-
-            i + 1 -> direita
-            i + n -> baixo
-            i + n + 1 -> direita inferior
-            i + n - 1 -> esquerda inferior
-
-            para os nós [5, 9] --> ligar apenas ao da direita, ao de baixo e ao direita inferior
-            para os nós [2, 3] --> ligar apenas ao da direita, baixo, direita inferior e esquerda inferior
-            para os nós [8, 12] --> ligar ao de baixo, esquerda inferior
-            para o nó 1 --> ligar direita, baixo, direita inferior
-            para o nó 4 --> ligar baixo, esquerda inferior
-            para o nó 13 --> ligar direita
-            para o nó 16 --> não ligar nada
-         
-        */
-        
-        
     public void readRelationship() {
-        
+
     }
 
     public void updateRelationship() {
-        
+
     }
 
-    // Deleta todas as relações de um nó em especifico
+    /**
+     * Função que apaga (DELETE) todas as Arestas de um determinado Vétice
+     * 
+     * @param id // id do Nó cujas Relações serão excluídas
+     */
     public void deleteRelationship(int id) {
         Session session = driver.session();
 
@@ -208,25 +227,28 @@ public class Connection {
 
         driver.close();
     }
-    
+
     public static void main(String[] args) {
         var app = new Connection();
 
+        // TESTES
+
         // for (int i = 0; i < 4; i++) {
-        //     for (int j = 0; j < 4; j++) {
-        //         int id = i * 4 + j;
-        //         int latitude = i;
-        //         int longitude = j;
-        //         int altitude = 0;
-        //         app.createNode(id, latitude, longitude, altitude);
-        //     }
+        // for (int j = 0; j < 4; j++) {
+        // int id = i * 4 + j;
+        // int latitude = i;
+        // int longitude = j;
+        // int altitude = 0;
+        // app.createNode(id, latitude, longitude, altitude);
+        // }
         // }
 
-        //app.createRelationship(4);
+        // app.createRelationship(4);
         // DtedDatabaseHandler dbRio = Coordinates.openDtedDB("dted/rio");
-        // double[][] coordData = Coordinates.getCoordData(dbRio, -43.4076, -22.1510, 5, 5);
+        // double[][] coordData = Coordinates.getCoordData(dbRio, -43.4076, -22.1510, 5,
+        // 5);
         // for(int i = 0; i < coordData.length; i++) {
-        //     app.createNode(i, coordData[i][0], coordData[i][1], coordData[i][2]);
+        // app.createNode(i, coordData[i][0], coordData[i][1], coordData[i][2]);
         // }
         app.createRelationship(5);
     }
--- src/main/java/br/edu/inteli/cc/m5/grupo/Coordinates.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo/Coordinates.java
@@ -5,38 +5,73 @@ import java.util.Optional;
 import br.edu.inteli.cc.m5.dted.DtedDatabaseHandler;
 
 public class Coordinates {
-
+  /**
+   * Essa função abre um determinado Banco de Dados DTED
+   * 
+   * @param path caminho do banco de dados
+   * @return retorna o banco de dados inicializado
+   */
   public static DtedDatabaseHandler openDtedDB(String path) {
-    DtedDatabaseHandler dtedDB = new DtedDatabaseHandler();
-    dtedDB.InitializeFromResources(path);
-    // if (dtedDBok)
+    DtedDatabaseHandler dtedDB = new DtedDatabaseHandler(); // variável do tipo DtedDatabaseHandler que armazena o DB
+    dtedDB.InitializeFromResources(path); // inicialização do DB através do caminho fornecido
+    // if (dtedDBok) --> verifica se o DB foi inicializado corretamnete (WIP!!!)
     return dtedDB;
   }
 
+  /**
+   * Função que retorna os dados necessários para montar um nó em forma de array
+   * 
+   * @param dbDTED    banco de dados DTED inicializado
+   * @param longitude longitude da coordenada
+   * @param latitude  latitude da coordenada
+   * @return array que contém longitude, latitude e altitude de um determinado
+   *         ponto
+   */
   public static double[] getAltitude(DtedDatabaseHandler dbDTED, double longitude, double latitude) {
 
-    Optional<Integer> altitude = dbDTED.QueryLatLonElevation(longitude, latitude);
-    //System.out.println("lon: " + longitude + ", lat: " + latitude + ", altitude: " + altitude.get());
+    Optional<Integer> altitude = dbDTED.QueryLatLonElevation(longitude, latitude); // query que busca a altitude de um
+                                                                                   // ponto com base na longitude e na
+                                                                                   // latitude da coordenada
+    // System.out.println("lon: " + longitude + ", lat: " + latitude + ", altitude:
+    // " + altitude.get());
 
-    double[] coordData = { longitude, latitude, (double) altitude.get() };
+    double[] coordData = { longitude, latitude, (double) altitude.get() }; // array contendo os dados de um Nó
+                                                                           // (longitude, latitude e altitude)
 
     return coordData;
   }
 
+  /**
+   * Função que retorna todas as informações para montar uma malha de nós com base
+   * na coordenada de um ponto inicial e
+   * no número de vértices dentro da malha
+   * Todos os pontos dentro dessa estrutura precisam estar dentro do DB DTED
+   * 
+   * @param dbDTED   banco de dados DTED inicializado
+   * @param longZero longitude do ponto inicial
+   * @param latZero  latitude do ponto inicial
+   * @param row      número de linhas da malha de nós
+   * @param col      número de colunas da malha de nós
+   * @return array que contém vários arrays com os dados necessários para a
+   *         construção de nós
+   */
   public static double[][] getCoordData(DtedDatabaseHandler dbDTED, double longZero, double latZero, int row, int col) {
-    double[][] coordData = new double[row * col][3];
+    double[][] coordData = new double[row * col][3]; // array multidimensional que contém as informações de todos os Nós
+                                                     // da malha
 
-    int count = 0;
+    int count = 0; // variável auxiliar que armazena a quantidade de nós criados
     double lon;
     double lat = latZero;
-    for (int i = 0; i < row; i++) {
+    for (int i = 0; i < row; i++) { // loop que obtém os dados linha por linha
       lon = longZero;
-      for (int j = 0; j < col; j++) {
+      for (int j = 0; j < col; j++) { // loop que obtém os dados de todos os nós de uma determinada linha da malha
         coordData[count] = getAltitude(dbDTED, lon, lat);
-        lon += 0.12; // rever constante
+        lon += 0.12; // constante (em graus) que viaja 120m no sentido Oeste - Leste em uma mesma
+                     // latitude com base na Fórmula de Harversine (WIP!!!)
         count++;
       }
-      lat -= 0.12; // rever constante
+      lat -= 0.0011; // constante (em graus) que viaja 120m no sentido Norte - Sul em uma mesma
+                     // logitude com base na Fórmula de Harversine (WIP!!!)
     }
 
     return coordData;
@@ -59,7 +94,7 @@ public class Coordinates {
       }
     }
 
-    //System.out.println(coordData);
+    // System.out.println(coordData);
 
   }
 }
