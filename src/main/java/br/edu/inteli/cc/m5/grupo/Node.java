d4f29ec9d1dddce37289a3a7767a5747fa10535a
mateusalm <mateus.almeida@sou.inteli.edu.br>
2023-03-08 16:20:05 -0300

    add node class

--- src/main/java/br/edu/inteli/cc/m5/grupo/Node.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo/Node.java
@@ -3,18 +3,36 @@ package br.edu.inteli.cc.m5.grupo;
 public class Node {
 
   private Vertex current;
-  private Node ancestor;
+  private Node parent;
   private double gCost;
+  private double fCost;
 
-  public Node(Vertex current, Node ancestor, boolean firstStep) {
+  public Node(Vertex current, Node parent, boolean firstStep, Edge connection, Vertex target) {
     this.current = current;
-    this.ancestor = ancestor;
+    this.parent = parent;
 
-    // this.gCost = ancestor.gCost +
+    this.gCost = parent.getgCost() + connection.getWeight();
+
+    this.fCost = this.gCost + Calculator.calcDist(current, target) + Calculator.altVar(current, target);
+
   }
 
   public double getgCost() {
     return gCost;
+  }
+
+  public double getfCost() {
+    return fCost;
+  }
+
+  public int compareTo(Node node) {
+    if (this.getfCost() > node.getfCost()) {
+      return 1;
+    } else if (this.getfCost() == node.getfCost()) {
+      return 0;
+    } else {
+      return -1;
+    }
   }
 
 }
--- src/main/java/br/edu/inteli/cc/m5/grupo/Star.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo/Star.java
@@ -17,10 +17,6 @@ public class Star {
 
     }
 
-    public double setInicialCost() {
-
-    }
-
     public void findPath() {
 
     }
